b6253b14e65ba29e700d5b51146eb47b9be1316c
Muhammad Arif <arifluqman477@gmail.com>
2024-09-07 23:02:00 +0500

    first comment statics resume completed

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+let nav = document.getElementById("nav") as HTMLUListElement;
+
+if (nav) {
+  const liElements = nav.querySelectorAll("li");
+
+  liElements.forEach((li, index) => {
+    li.addEventListener("click", () => {
+      liElements.forEach((li) => {
+        li.id = "";
+      });
+      const sections = document.querySelectorAll(`section`);
+      if (sections) {
+        sections.forEach((section) => {
+          section.style.display = "none";
+          if (section.classList.contains(li.innerText.toLowerCase())) {
+            section.style.display = "grid";
+            li.id = "active";
+          }
+        });
+      }
+    });
+  });
+} else {
+  console.error("The <ul> element was not found.");
+}
